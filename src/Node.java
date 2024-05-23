eb087b5ab214c9e062f77d2c8df267fe70d1360c
Milo <milojevic.luka2002@gmail.com>
2024-05-24 00:37:40 +0200

    Implemented Node class

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Node {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    String value;
+    List<Edge> edges;
+
+    public Node(String value) {
+        this.value = value;
+        this.edges = new ArrayList<>();
+    }
 }
