6e428733b88be51f7fc8b4317cd5dba9958759c8
Milo <milojevic.luka2002@gmail.com>
2024-05-24 00:37:25 +0200

    Implemented Edge class

---
+++ src/Edge.java
@@ -0,0 +1,11 @@
+public class Edge {
+    Node source;
+    Node destination;
+    int weight;
+
+    public Edge(Node source, Node destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
