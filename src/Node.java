2478bb0121f3affd6ec525c25a23314af2521c93
Milo <milojevic.luka2002@gmail.com>
2024-05-24 00:42:38 +0200

    Implemented method for creating edges bidirectionaly

--- src/Node.java
+++ src/Node.java
@@ -9,4 +9,24 @@ public class Node {
         this.value = value;
         this.edges = new ArrayList<>();
     }
+
+    public void addEdgeBidirectional(Node destination, int weight) {
+        if (!isEdgePresent(destination)) {
+            Edge edge = new Edge(this, destination, weight);
+            Edge reverseEdge = new Edge(destination, this, weight);
+
+            this.edges.add(edge);
+            destination.edges.add(reverseEdge);
+        }
+
+    }
+
+    private boolean isEdgePresent(Node destination) {
+        for (Edge edge : this.edges) {
+            if (edge.destination == destination) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
