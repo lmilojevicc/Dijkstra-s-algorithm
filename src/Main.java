fee8150218ad4d37bfd071d449b00a19fbe5698a
Milo <milojevic.luka2002@gmail.com>
2024-05-24 12:49:03 +0200

    Created a function to print out graph as linked list

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,23 @@ public class Main {
     }
 
     private static void printLinkedList() {
+        System.out.println("Graph as Linked list:");
+        for (Node node : nodes) {
+            System.out.print(node.value + ". ");
+            if (node.edges.isEmpty()) {
+                System.out.println("null");
+            } else {
+                for (int i = 0; i < node.edges.size(); i++) {
+                    Edge edge = node.edges.get(i);
+                    System.out.print("(" + edge.destination.value + "," + edge.weight + ")");
+                    if (i < node.edges.size() - 1) {
+                        System.out.print("->");
+                    } else {
+                        System.out.println("->null");
+                    }
+                }
+            }
+        }
     }
 
     private static void findShortestPathsFromSource() {
